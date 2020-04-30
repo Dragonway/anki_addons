1577231770998aa36a84547c0efa1287e10281cd
Ivan Leonov <ivan.leonov.d@yandex.com>
2020-04-30 15:45:29 +0300

    Add whole note's stylsheet to editor's page (instead of manual css parsing), control fields styling thorugh classes
    	it allow us keep style of fields tied to card' template and use else styles for manual styling of some text parts in any fields
    
    	just make things simpler

--- styler/styler.py
+++ styler/styler.py
@@ -27,38 +27,12 @@ def add_styler_shortcut(shortcuts : List[Tuple], _ : Editor):
     )
 
 
-CSS_PROP_SEP = ':'
-CSS_PROP_END = ';'
-
-def css_props_from_text(props_text : str) -> Dict[str, str]:
-    props = dict()
-
-    prop_begin = 0
-    prop_sep = props_text.find(CSS_PROP_SEP, prop_begin)
-    prop_end = props_text.find(CSS_PROP_END, prop_sep)
-
-    while prop_sep >= 0:
-        prop_name = props_text[prop_begin:prop_sep].strip()
-        prop_value = props_text[prop_sep+1:prop_end].strip()
-
-        props[prop_name] = prop_value
-
-        prop_begin = prop_end + 1
-        prop_sep = props_text.find(CSS_PROP_SEP, prop_begin)
-        prop_end = props_text.find(CSS_PROP_END, prop_sep)
-
-    return props
-
-
 BODY_CLASS = 'card'
 TEMPLATES_KEY = 'tmpls'
 FRONT_SIDE_KEY = 'qfmt'
 BACK_SIDE_KEY = 'afmt'
 
 CARD_ORD = 0  # TODO: Implement card switching
-
-# TODO : Skip commented lines
-RE_CSS_CLASS = re_compile("\.(?P<class>\w+)\s*\{\s*(?P<props>[^\}]*)\}")
 
 def style_note_fields(editor : Editor):
     note_css : str = editor.note.model()['css']
@@ -70,32 +44,22 @@ def style_note_fields(editor : Editor):
 
     full_card = f"<div>{front_side}<br>{back_side}</div>"
 
-    # TODO: Support type selectors and its combinations with class
-    css_rules = {t[0] : css_props_from_text(t[1]) for t in RE_CSS_CLASS.findall(note_css)}
-
-    # TODO: Support multiple class selection
     editor.web.eval(
 f'''
-        let card = $(`{full_card}`)
-        let css = {str(css_rules)}
-        let body_css_rule = css["{BODY_CLASS}"];
+        let card = $(`{full_card}`);
+        $("head").append(`<style type="text/css">{note_css}</style>`);
+
         let fields = {str(note_fields)};
         for (let i = 0; i < fields.length; ++i) {{
             let field = $(`#f${{i}}`);
             let field_name = fields[i];
             
-            if (body_css_rule)
-                field.css(body_css_rule);
+            field.addClass("{BODY_CLASS}");
 
             let elem = card.find(`div:contains("{{{{${{field_name}}}}}}")`);
-
             let elem_class = elem.attr("class");
-            if (!elem_class)
-                continue;
-
-            let css_rule = css[elem_class]
-            if (css_rule)
-                field.css(css_rule);
+            
+            field.addClass(elem_class);
         }}
 '''
     )
