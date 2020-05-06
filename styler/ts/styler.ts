94807bdd0df385f137ed80365544e7f482b1dae4
Ivan Leonov <ivan.leonov.d@yandex.com>
2020-05-06 22:50:24 +0300

    Introduce shadeRgb func

--- styler/ts/styler.ts
+++ styler/ts/styler.ts
@@ -3,6 +3,20 @@ namespace StylerAddon {
     const BODY_CLASS    = 'card';
     const STYLE_ELEM_ID = '__styler_note_css';
 
+    function shadeRgb(p: number, rgb: string): string {
+        const begin = rgb.indexOf('(') + 1;
+        const end = rgb.length - 1;
+
+        const [r, g, b, ...a] = rgb.substring(begin, end).split(',');
+
+        const shade = (p: number, x: string): number => { return Math.round(parseInt(x) * p); };
+        const tint  = (p: number, x: string): number => { return Math.round(255 - (255 - parseInt(x)) * p); };
+
+        const conv = p < 0 ? tint.bind(null, 1 + Math.max(p, -1)) : shade.bind(null, 1 - Math.min(p, 1));
+
+        return `${rgb.substring(0, begin-1)}(${conv(r)}, ${conv(g)}, ${conv(b)}${a.length > 0 ? ', ' + a[0] : ''})`;
+    }
+
     export function styleNoteFields(fullCardHtml: string, noteCss: string, noteFields: string[]): void {
         let card = $(fullCardHtml);
 
