c19363730d0e7f6175cc24931cbc5bfa485a3245
Ivan Leonov <ivan.leonov.d@yandex.com>
2020-05-09 01:04:49 +0300

    Move 'shadeRgb' func to end of file, mark this section as 'Helpers'

--- styler/ts/styler.ts
+++ styler/ts/styler.ts
@@ -9,19 +9,6 @@ namespace StylerAddon {
     const SELECT_LIST_UNFOLDED_CLASS    = '__styler_select_list_unfolded';
     const DROP_DOWN_LIST_CLASS          = '__styler_drop_down_list';
 
-    function shadeRgb(p: number, rgb: string): string {
-        const begin = rgb.indexOf('(') + 1;
-        const end = rgb.length - 1;
-
-        const [r, g, b, ...a] = rgb.substring(begin, end).split(',');
-
-        const shade = (p: number, x: string): number => { return Math.round(parseInt(x) * p); };
-        const tint  = (p: number, x: string): number => { return Math.round(255 - (255 - parseInt(x)) * p); };
-
-        const conv = p < 0 ? tint.bind(null, 1 + Math.max(p, -1)) : shade.bind(null, 1 - Math.min(p, 1));
-
-        return `${rgb.substring(0, begin-1)}(${conv(r)}, ${conv(g)}, ${conv(b)}${a.length > 0 ? ', ' + a[0] : ''})`;
-    }
 
     export function styleNoteFields(fullCardHtml: string, noteCss: string, noteFields: string[]): void {
         let card = $(fullCardHtml);
@@ -228,6 +215,23 @@ namespace StylerAddon {
         private static optionUnfocused(this: HTMLElement): void {
             $(this).css('background', "rgba(0,0,0,0)");
         }
+    }
+
+
+    // Helpers
+
+    function shadeRgb(p: number, rgb: string): string {
+        const begin = rgb.indexOf('(') + 1;
+        const end = rgb.length - 1;
+
+        const [r, g, b, ...a] = rgb.substring(begin, end).split(',');
+
+        const shade = (p: number, x: string): number => { return Math.round(parseInt(x) * p); };
+        const tint  = (p: number, x: string): number => { return Math.round(255 - (255 - parseInt(x)) * p); };
+
+        const conv = p < 0 ? tint.bind(null, 1 + Math.max(p, -1)) : shade.bind(null, 1 - Math.min(p, 1));
+
+        return `${rgb.substring(0, begin-1)}(${conv(r)}, ${conv(g)}, ${conv(b)}${a.length > 0 ? ', ' + a[0] : ''})`;
     }
 
 }
